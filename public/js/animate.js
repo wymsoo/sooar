30f3edcb7409de320184a08346d5f69b9028063a
HarryPoon000 <poontszhang@gmail.com>
2025-07-30 14:29:59 +0800

    hack: cleanup animations

--- public/js/animate.js
+++ public/js/animate.js
@@ -5,15 +5,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
     //     rootMargin: '0px 0px 0% 0px', // Trigger when the element is 50% in view
     //     threshold: 0 // Trigger as soon as any part of the element is visible
     // };
-
+    const tracker = {}
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry =>{
             if (entry.isIntersecting){
+                if (entry.target in tracker){
+                    clearInterval(tracker[entry.target]);
+                    delete tracker[entry.target];
+                }
                 entry.target.classList.add('in-view');
                 return
+            } else {
+                if (entry.target in tracker){
+                    clearInterval(tracker[entry.target]);
+                    delete tracker[entry.target];
+                }
+                tracker[entry.target] = setTimeout(()=>{
+                    entry.target.classList.remove('in-view');
+                }, 500)
             }
-                entry.target.classList.remove('in-view');
-
             
         });
     });
