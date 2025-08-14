53fb0a6e99d1aa3575d0970028819317c5003859
Minnie Soo <wymsoo@connect.ust.hk>
2025-08-15 01:05:45 +0800

    change stripe return url

--- routes/checkout.js
+++ routes/checkout.js
@@ -23,7 +23,7 @@ router.post('/create-checkout-session', async (req, res) => {
     }],
     mode: 'payment',
     ui_mode: 'embedded',
-    return_url: `http://localhost:8080/checkout/checkoutReturn?session_id={CHECKOUT_SESSION_ID}&amount=${String(prod.price)}&id=${prod.id}&type=${prod.type}&purchaseid=${purchaseid}`
+    return_url: `/checkout/checkoutReturn?session_id={CHECKOUT_SESSION_ID}&amount=${String(prod.price)}&id=${prod.id}&type=${prod.type}&purchaseid=${purchaseid}`
     //redirect_on_completion: 'never'
 
   });
